16ec43898ea995dcdb534442b3628dfc1ea4b306
Joe DeCapo <joe@polka.cat>
2022-01-08 16:44:46 -0600

    wip

--- script/import.sh
+++ script/import.sh
@@ -1,28 +1,39 @@
 # shellcheck shell=bash
 
-# Homebrew
-intel_brew_path='/usr/local/bin/brew'
-m1_brew_path='/opt/homebrew/bin/brew'
-# Export Homebrew variables for current platform
-if [[ -x "$intel_brew_path" ]]; then
-    eval "$($intel_brew_path shellenv)"
-elif [[ -x "$m1_brew_path" ]]; then
-    eval "$($m1_brew_path shellenv)"
-fi
+case "$PLATFORM" in
+    darwin)
+        h1 'Homebrew'
+        # Homebrew
+        intel_brew_path='/usr/local/bin/brew'
+        m1_brew_path='/opt/homebrew/bin/brew'
+        # Export Homebrew variables for current platform
+        if [[ -x "$intel_brew_path" ]]; then
+            eval "$($intel_brew_path shellenv)"
+        elif [[ -x "$m1_brew_path" ]]; then
+            eval "$($m1_brew_path shellenv)"
+        fi
 
-# Homebrew Python
-homebrew_python_path='/usr/local/opt/python/libexec/bin'
-export PATH="$homebrew_python_path:$PATH"
+        # Homebrew Python
+        homebrew_python_path='/usr/local/opt/python/libexec/bin'
+        export PATH="$homebrew_python_path:$PATH"
 
-# Pipenv
-local_python_bin="$HOME/.local/bin"
-export PATH="$local_python_bin:$PATH"
-user_python_bin="$(python -m site --user-base)/bin"
-export PATH="$user_python_bin:$PATH"
+        # Pipenv
+        local_python_bin="$HOME/.local/bin"
+        export PATH="$local_python_bin:$PATH"
+        user_python_bin="$(python -m site --user-base)/bin"
+        export PATH="$user_python_bin:$PATH"
 
-# pyenv
-if command -v pyenv 1>/dev/null 2>&1; then eval "$(pyenv init -)" || true; fi
+        # pyenv
+        if command -v pyenv 1>/dev/null 2>&1; then eval "$(pyenv init -)" || true; fi
 
-# PYTHONPATH
-repo_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
-export PYTHONPATH="${repo_dir}:${PYTHONPATH-}"
+        # PYTHONPATH
+        repo_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
+        export PYTHONPATH="${repo_dir}:${PYTHONPATH-}"
+        ;;
+    linux)
+        exit_failure 'Windows is not currently supported'
+        ;;
+    windows)
+        exit_failure 'Windows is not currently supported'
+        ;;
+esac
